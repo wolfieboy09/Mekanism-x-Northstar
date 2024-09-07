fabdca34eafdaed26ab15fe4f1cff10c13f2c510
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-09-06 20:51:06 -0400

    add lang

--- src/main/java/dev/wolfieboy09/mek_x_star/registries/MNItems.java
+++ src/main/java/dev/wolfieboy09/mek_x_star/registries/MNItems.java
@@ -1,7 +1,6 @@
 package dev.wolfieboy09.mek_x_star.registries;
 
 import dev.wolfieboy09.mek_x_star.MekanismNorthStar;
-import dev.wolfieboy09.mek_x_star.item.ModuleSpaceSuitItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -11,5 +10,4 @@ public class MNItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MekanismNorthStar.MOD_ID);
 
     public static final RegistryObject<Item> MODULE_SPACE_SUIT = ITEMS.register("module_space_suit",() -> new Item(new Item.Properties()));
-
 }
