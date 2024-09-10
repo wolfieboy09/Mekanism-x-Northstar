dad4c8c6115592d94159b655a421dfc0cec43b02
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-09-10 16:58:06 -0400

    Fix bug where the NBT would reset, causing everything to be lost

--- src/main/java/dev/wolfieboy09/mek_x_star/modules/ModuleOxygenTank.java
+++ src/main/java/dev/wolfieboy09/mek_x_star/modules/ModuleOxygenTank.java
@@ -2,29 +2,21 @@ package dev.wolfieboy09.mek_x_star.modules;
 
 import mekanism.api.gear.ICustomModule;
 import mekanism.api.gear.IModule;
-import mekanism.api.gear.config.ModuleConfigItemCreator;
-import net.minecraft.nbt.CompoundTag;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.Objects;
 
 @ParametersAreNonnullByDefault
 public class ModuleOxygenTank implements ICustomModule<ModuleOxygenTank> {
-    private final String IS_INSTALLED_TAG_NAME = "isOxygenTankInstalled";
-    @Override
-    public void init(IModule<ModuleOxygenTank> module, ModuleConfigItemCreator configItemCreator) {
-        ICustomModule.super.init(module, configItemCreator);
-    }
-
     @Override
     public void onAdded(IModule<ModuleOxygenTank> module, boolean first) {
         ICustomModule.super.onAdded(module, first);
-        Objects.requireNonNull(module.getContainer().getOrCreateTag()).putBoolean(IS_INSTALLED_TAG_NAME, true);
+        Objects.requireNonNull(module.getContainer().getOrCreateTag()).putBoolean("isOxygenTankInstalled", true);
     }
 
     @Override
     public void onRemoved(IModule<ModuleOxygenTank> module, boolean last) {
         ICustomModule.super.onRemoved(module, last);
-        Objects.requireNonNull(module.getContainer().getOrCreateTag()).putBoolean(IS_INSTALLED_TAG_NAME, false);
+        Objects.requireNonNull(module.getContainer().getOrCreateTag()).putBoolean("isOxygenTankInstalled", false);
     }
 }
--- src/main/java/dev/wolfieboy09/mek_x_star/modules/ModuleSpaceSuit.java
+++ src/main/java/dev/wolfieboy09/mek_x_star/modules/ModuleSpaceSuit.java
@@ -3,29 +3,20 @@ package dev.wolfieboy09.mek_x_star.modules;
 import mekanism.api.annotations.NothingNullByDefault;
 import mekanism.api.gear.ICustomModule;
 import mekanism.api.gear.IModule;
-import mekanism.api.gear.config.ModuleConfigItemCreator;
-import net.minecraft.nbt.CompoundTag;
 
 import java.util.Objects;
 
 @NothingNullByDefault
 public class ModuleSpaceSuit implements ICustomModule<ModuleSpaceSuit> {
-    private static final String IS_INSTALLED_TAG_NAME = "isSpaceModuleInstalled";
-
-    @Override
-    public void init(IModule<ModuleSpaceSuit> module, ModuleConfigItemCreator configItemCreator) {
-        ICustomModule.super.init(module, configItemCreator);
-    }
-
     @Override
     public void onAdded(IModule<ModuleSpaceSuit> module, boolean first) {
         ICustomModule.super.onAdded(module, first);
-        Objects.requireNonNull(module.getContainer().getOrCreateTag()).putBoolean(IS_INSTALLED_TAG_NAME, true);
+        Objects.requireNonNull(module.getContainer().getOrCreateTag()).putBoolean("isSpaceModuleInstalled", true);
     }
 
     @Override
     public void onRemoved(IModule<ModuleSpaceSuit> module, boolean last) {
         ICustomModule.super.onRemoved(module, last);
-        Objects.requireNonNull(module.getContainer().getOrCreateTag()).putBoolean(IS_INSTALLED_TAG_NAME, false);
+        Objects.requireNonNull(module.getContainer().getOrCreateTag()).putBoolean("isSpaceModuleInstalled", false);
     }
 }
