de1fdaa5326f5b07b935732d5620dd432b13a8a5
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-09-10 17:40:10 -0400

    Create oxygen tank module texture

--- src/main/java/dev/wolfieboy09/mek_x_star/modules/ModuleHeatResistive.java
+++ src/main/java/dev/wolfieboy09/mek_x_star/modules/ModuleHeatResistive.java
@@ -2,20 +2,19 @@ package dev.wolfieboy09.mek_x_star.modules;
 
 import mekanism.api.gear.ICustomModule;
 import mekanism.api.gear.IModule;
-import mekanism.api.gear.config.ModuleConfigItemCreator;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
 @ParametersAreNonnullByDefault
-public class ModuleHeatResistant implements ICustomModule<ModuleHeatResistant> {
+public class ModuleHeatResistive implements ICustomModule<ModuleHeatResistive> {
     @Override
-    public void onAdded(IModule<ModuleHeatResistant> module, boolean first) {
+    public void onAdded(IModule<ModuleHeatResistive> module, boolean first) {
         ICustomModule.super.onAdded(module, first);
         module.getContainer().getOrCreateTag().putBoolean("isHeatResistant", true);
     }
 
     @Override
-    public void onRemoved(IModule<ModuleHeatResistant> module, boolean last) {
+    public void onRemoved(IModule<ModuleHeatResistive> module, boolean last) {
         ICustomModule.super.onRemoved(module, last);
         module.getContainer().getOrCreateTag().putBoolean("isHeatResistant", false);
     }
