5ac3790d77e5bde2fee24f2c8fa8b24603731515
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-11-26 17:20:50 -0500

    remove unused import

--- src/main/java/dev/wolfieboy09/mek_x_star/MekanismNorthStar.java
+++ src/main/java/dev/wolfieboy09/mek_x_star/MekanismNorthStar.java
@@ -9,7 +9,6 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 
 @Mod(MekanismNorthStar.MOD_ID)
