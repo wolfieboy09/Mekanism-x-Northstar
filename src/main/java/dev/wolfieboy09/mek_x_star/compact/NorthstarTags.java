7b3107e84b84b4cbd0a19ac398300c653509a089
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-11-18 16:24:44 -0500

    actually start working on the mod again to port it to NS Redux

--- src/main/java/dev/wolfieboy09/mek_x_star/compact/NorthstarTags.java
+++ src/main/java/dev/wolfieboy09/mek_x_star/compact/NorthstarTags.java
@@ -6,8 +6,8 @@ import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 
 public class NorthstarTags {
-    public static final TagKey<Item> OXYGEN_SEALING = TagKey.create(Registries.ITEM, new ResourceLocation("northstar:oxygen_sealing"));
-    public static final TagKey<Item> OXYGEN_SOURCES = TagKey.create(Registries.ITEM, new ResourceLocation("northstar:oxygen_sources"));
-    public static final TagKey<Item> INSULATING = TagKey.create(Registries.ITEM, new ResourceLocation("northstar:insulating"));
-    public static final TagKey<Item> HEAT_RESISTANT = TagKey.create(Registries.ITEM, new ResourceLocation("northstar:heat_resistant"));
+    public static final TagKey<Item> OXYGEN_SEALING = TagKey.create(Registries.ITEM, ResourceLocation.parse("northstar:oxygen_sealing"));
+    public static final TagKey<Item> OXYGEN_SOURCES = TagKey.create(Registries.ITEM, ResourceLocation.parse("northstar:oxygen_sources"));
+    public static final TagKey<Item> INSULATING = TagKey.create(Registries.ITEM, ResourceLocation.parse("northstar:insulating"));
+    public static final TagKey<Item> HEAT_RESISTANT = TagKey.create(Registries.ITEM, ResourceLocation.parse("northstar:heat_resistant"));
 }
