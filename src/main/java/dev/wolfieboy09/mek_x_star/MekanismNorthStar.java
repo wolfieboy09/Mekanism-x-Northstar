c142de58d89ff3f6df70f30efa012f68c94f3c72
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-11-23 20:17:42 -0500

    Change to a new main class

---
+++ src/main/java/dev/wolfieboy09/mek_x_star/MekanismNorthStar.java
@@ -0,0 +1,25 @@
+package dev.wolfieboy09.mek_x_star;
+
+import dev.wolfieboy09.mek_x_star.registries.MNDataComponents;
+import dev.wolfieboy09.mek_x_star.registries.MNItems;
+import dev.wolfieboy09.mek_x_star.registries.MNModules;
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.ModContainer;
+
+@Mod(MekanismNorthStar.MOD_ID)
+public class MekanismNorthStar {
+    public static final String MOD_ID = "mek_x_star";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public MekanismNorthStar(IEventBus modEventBus, ModContainer modContainer) {
+        LOGGER.info("Mekanism? In space? HILARIOUS");
+        MNDataComponents.REGISTAR.register(modEventBus);
+        MNItems.ITEMS.register(modEventBus);
+        MNModules.MODULES.register(modEventBus);
+    }
+}
