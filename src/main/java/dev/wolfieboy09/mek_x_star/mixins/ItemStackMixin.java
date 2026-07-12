602c5e72b868061c9e4da3b2d08325d580c0db3d
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2026-07-11 21:22:32 -0400

    Improve ItemStackMixin

--- src/main/java/dev/wolfieboy09/mek_x_star/mixins/ItemStackMixin.java
+++ src/main/java/dev/wolfieboy09/mek_x_star/mixins/ItemStackMixin.java
@@ -25,25 +25,25 @@ public abstract class ItemStackMixin implements DataComponentHolder, MutableData
     private void reportFakeTags(TagKey<Item> tag, CallbackInfoReturnable<Boolean> cir) {
         ItemStack self = mekanismXNorthstar$self();
 
-        // Space suit unit.
-        // Uninstalling that unit makes the suit fine??
-        // Well, let's just do this
-        if (self.has(MNDataComponents.OXYGEN_SEALING)) {
-            if (tag == NorthstarTags.NorthstarItemTags.INSULATING.tag() && self.has(MNDataComponents.INSULATING)) {
-                cir.setReturnValue(true);
-            }
+        // Space suit unit
+        if (!self.has(MNDataComponents.OXYGEN_SEALING)) return;
 
-            if (tag == NorthstarTags.NorthstarItemTags.OXYGEN_SEALING.tag() && self.has(MNDataComponents.OXYGEN_SEALING)) {
-                cir.setReturnValue(true);
-            }
 
-            if (tag == NorthstarTags.NorthstarItemTags.HEAT_RESISTANT.tag() && self.has(MNDataComponents.HEAT_RESISTANT)) {
-                cir.setReturnValue(true);
-            }
+        if (tag == NorthstarTags.NorthstarItemTags.INSULATING.tag() && self.has(MNDataComponents.INSULATING)) {
+            cir.setReturnValue(true);
+        }
 
-            if (tag == NorthstarTags.NorthstarItemTags.OXYGEN_SOURCES.tag() && self.has(MNDataComponents.OXYGEN_SOURCE)) {
-                cir.setReturnValue(true);
-            }
+        // Does not need self.has due to the initial if statement checking that already
+        if (tag == NorthstarTags.NorthstarItemTags.OXYGEN_SEALING.tag()) {
+            cir.setReturnValue(true);
+        }
+
+        if (tag == NorthstarTags.NorthstarItemTags.HEAT_RESISTANT.tag() && self.has(MNDataComponents.HEAT_RESISTANT)) {
+            cir.setReturnValue(true);
+        }
+
+        if (tag == NorthstarTags.NorthstarItemTags.OXYGEN_SOURCES.tag() && self.has(MNDataComponents.OXYGEN_SOURCE)) {
+            cir.setReturnValue(true);
         }
     }
 }
